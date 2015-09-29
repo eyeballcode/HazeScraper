1ba5adc5db865f1346d386e4e68f34c9ac8d6f21
Eyeballcode <edwardyeung.private@gmail.com>
2015-09-29 17:40:41 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,117 @@
+import com.sun.awt.AWTUtilities;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Main {
+    // jQuery for selector the PSI vals.
+    // $($('table.noalter')[1]).children().children().not('.even').children()
+
+    static int pX;
+    static int pY = 0;
+
+    public static void main(String[] a) throws IOException {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception exp) {
+        }
+
+        final JDialog main = new JDialog();
+//        final JFrame main = new JFrame();
+        main.setTitle("Haze!");
+        JRootPane root = main.getRootPane();
+        root.putClientProperty("Window.shadow", root);
+        main.setPreferredSize(new Dimension(190, 30));
+        final JLabel label = new JLabel("Please wait..");
+        main.add(label);
+        main.setResizable(false);
+        main.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        main.setLocation(0, 0);
+        main.setAlwaysOnTop(true);
+        main.setUndecorated(true);
+        main.setOpacity(0.7f);
+        main.setBackground(Color.WHITE);
+//        AWTUtilities.setWindowOpaque(main, false);
+        main.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                pX = me.getX();
+                pY = me.getY();
+
+            }
+
+            public void mouseDragged(MouseEvent me) {
+                main.setLocation(main.getLocation().x + me.getX() - pX,
+                        main.getLocation().y + me.getY() - pY);
+            }
+        });
+
+        main.addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent me) {
+                main.setLocation(main.getLocation().x + me.getX() - pX,
+                        main.getLocation().y + me.getY() - pY);
+            }
+        });
+        KeyStroke close = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
+        Action action = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                int close = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Confimation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                if (close == 0)
+                    System.exit(0);
+            }
+        };
+        root
+                .getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(close, "ESCAPE");
+        root.getActionMap().put("ESCAPE", action);
+        main.pack();
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Document document = Jsoup.connect("http://www.haze.gov.sg/haze-updates/psi-readings-over-the-last-24-hours").get();
+                        Element hour12 = document.select(".noalter").get(1).children().select("tr").not(".even").get(0);
+                        Element hour24 = document.select(".noalter").get(1).children().select("tr").not(".even").get(1);
+                        main.revalidate();
+                        int hour = GregorianCalendar.getInstance().get(Calendar.HOUR_OF_DAY);
+                        int timeIn12Hour = GregorianCalendar.getInstance().get(Calendar.HOUR);
+                        if (hour > 12) {
+                            String psi24 = hour24.select("td").get(hour - 12).text();
+                            if (psi24.equals("-")) {
+                                psi24 = hour24.select("td").get(hour - 13).text();
+                                timeIn12Hour--;
+                            }
+                            label.setText("  As of " + timeIn12Hour + "pm, the PSI is " + psi24);
+                            main.repaint();
+                            System.out.println("As of " + timeIn12Hour + "pm, the PSI is " + psi24);
+                        } else {
+                            String psi12 = hour12.select("td").get(hour - 3).text();
+                            if (psi12.equals("-")) {
+                                psi12 = hour24.select("td").get(hour - 4).text();
+                                timeIn12Hour--;
+                            }
+
+                            label.setText("  As of " + timeIn12Hour + "am, the PSI is " + psi12);
+                            System.out.println("As of " + timeIn12Hour + "am, the PSI is " + psi12);
+                        }
+                        main.pack();
+                        main.repaint();
+                        Thread.sleep(10000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+//        };
+        }.start();
+        main.setVisible(true);
+    }
+}
+
